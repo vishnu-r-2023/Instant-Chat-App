dc8e421dc1ffffafc288b10f0874cd4b1dcd575c
vishnu-r-2023 <153156058+vishnu-r-2023@users.noreply.github.com>
2025-01-31 01:32:26 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	// eslint-disable-next-line no-undef
-	plugins: [require("daisyui")],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 };
